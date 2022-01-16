9dabef91da43ad8c07a33f5c9df9175b50bce379
Jonathan Dahan <git@jonathan.is>
2022-01-16 15:15:57 -0500

    Make MMRs and startingMMR editable

--- index.tsx
+++ index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import lines from "./log.js";
@@ -10,16 +10,6 @@ const HeroIDs: Record<string, Hero> = {
   '5e525924-1173-46c8-89c6-4729777818df': "Merlin",
 }
 
-const records: Game[] = lines
-  .map(({Hero: { Card: { ID } }, Placement }) => ({
-     hero: HeroIDs[ID] ?? 'Unknown',
-     placement: Placement,
-     mmr: Math.round(-100 + (25 * (8-Placement)) - 10 + (Math.random() * 20))
-    })
-  )
-
-const starting = 5396;
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -28,7 +18,33 @@ ReactDOM.render(
 );
 
 function App() {
-  const current = records.reduce((sum, { mmr }) => mmr + sum, starting);
+  const initialRecords: Game[] = lines
+    .map(({Hero: { Card: { ID } }, Placement }) => ({
+       hero: HeroIDs[ID] ?? 'Unknown',
+       placement: Placement,
+       mmr: Math.round(-100 + (25 * (8-Placement)) - 10 + (Math.random() * 20))
+      })
+    )
+  const [records, setRecords] = useState(initialRecords)
+
+  const [startingMMR, _setStartingMMR] = useState(0)
+  const setStartingMMR = (event) => void _setStartingMMR(parseInt(event.target.innerText))
+
+  const setMMR = (index: number, mmr: number) => {
+    console.log(index, mmr, records[index])
+    const record = {
+      hero: records[index].hero,
+      placement: records[index].placement,
+      mmr
+    }
+
+    const newRecords = [...records.slice(0, index), record, ...records.slice(index+1)]
+    console.log(newRecords)
+    setRecords(newRecords)
+  }
+
+  const current = records.reduce((sum, { mmr }) => mmr + sum, startingMMR);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -38,21 +54,26 @@ function App() {
             flexDirection: "row",
             display: "flex",
             flex: 1,
+	    width: 300,
             justifyContent: "space-between",
           }}
         >
           <span>
-            <h4>starting</h4>
-            <div>{starting}</div>
+            <h2>starting</h2>
+            <h1 contentEditable={true} onBlur={setStartingMMR}>{startingMMR}</h1>
           </span>
           <span>
-            <h4>current</h4>
-            <div>{current}</div>
+            <h2>current</h2>
+            <h1>{current}</h1>
           </span>
         </div>
 
         <h2>scores</h2>
-        <div style={{ flexDirection: "column" }}>{records.map(Record)}</div>
+        <div style={{ flexDirection: "column" }}>
+	  {records.map((recordProps, index) => (
+	    <Record key={index} {...recordProps} setMMR={(mmr) => setMMR(index, mmr)} />
+	  ))}
+        </div>
       </header>
     </div>
   );
@@ -69,10 +90,12 @@ interface Game {
   mmr: Mmr;
 }
 
-function Record({ hero, placement, mmr }: Game, index: number) {
+function Record({ hero, placement, mmr, setMMR }: Game, index: number) {
   const heroUri = `/assets/cards/SBB_HERO_${hero.toUpperCase()}.png`;
   const fontSize = 80;
   const textShadow = "6px 6px 2px black";
+
+  const updateMMR = (event) => void setMMR(parseInt(event.target.innerText))
 
   return (
     <div
@@ -99,7 +122,8 @@ function Record({ hero, placement, mmr }: Game, index: number) {
       </span>
 
       <span
-        className="mmr"
+        contentEditable={true}
+	onBlur={updateMMR}
         style={{
           fontSize,
           textShadow,
