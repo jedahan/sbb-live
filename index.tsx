f34445956b27e6f8e6b0bcf7482f4086dcac5c11
micro <not-today-twitch@example.com>
2022-01-26 18:20:49 -0500

    factor some hooks out of the main App component

--- index.tsx
+++ index.tsx
@@ -8,8 +8,8 @@ import defaultLog from './Player.log.txt'
 // @ts-expect-error
 import templateIds from './build/SBBTracker/assets/template-ids.json'
 
-const StartingMMR: React.FC<{onChange: (mmr: number) => void}> = ({ onChange }) => {
-  const inputRef = useRef<HTMLInputElement|null>(null)
+const StartingMMR: React.FC<{ onChange: (mmr: number) => void }> = ({ onChange }) => {
+  const inputRef = useRef<HTMLInputElement | null>(null)
 
   const setStartingMMR = (): void => {
     if (inputRef?.current == null) return
@@ -37,56 +37,70 @@ const StartingMMR: React.FC<{onChange: (mmr: number) => void}> = ({ onChange }) 
   )
 }
 
-const CurrentMMR: React.FC<{mmr: number}> = ({ mmr }) => (
-  <span style={{ padding: '20px' }}>
-    <label htmlFor='currentMMR'>current mmr</label>
-    <input
-      id='currentMMR'
-      type='number' min={0} max={9999} step={100}
-      value={mmr}
-      readOnly
-    />
-  </span>
-)
+const CurrentMMR: React.FC<{ startingMMR: number, records?: Game[] }> = ({ startingMMR, records }) => {
+  const [currentMMR, setCurrentMMR] = useState(0)
 
-function App (): ReactElement {
-  const [records, setRecords] = useState<Game[]>()
-  const [logFile, setLogFile] = useState<string>()
+  useEffect(() => {
+    if (records == null) return
+    setCurrentMMR(records?.reduce((sum, { mmr }) => mmr + sum, startingMMR))
+  }, [records, startingMMR])
+
+  return (
+    <span style={{ padding: '20px' }}>
+      <label htmlFor='currentMMR'>current mmr</label>
+      <input
+        id='currentMMR'
+        type='number' min={0} max={9999} step={100}
+        value={currentMMR}
+        readOnly
+      />
+    </span>
+  )
+}
+
+const useLogpath = async () => {
+  const logfile = 'Player.log'
+
+  const platform = await os.platform()
+  if (platform !== 'windows') return logfile
+
+  const datadir = (await path.dataDir()).replace('Roaming', 'LocalLow')
+  return [datadir, 'Good Luck Games', 'Storybook Brawl', logfile].join(path.sep)
+}
+
+const useReadloop = (path: string, pollseconds: number) => {
   const [text, setText] = useState<string>(defaultLog)
-  const [logTimeout, setLogTimeout] = useState<NodeJS.Timer>()
-  const [startingMMR, setStartingMMR] = useState(0)
-  const [current, setCurrent] = useState(0)
 
-  const [platform, setPlatform] = useState<string>()
-
-  os.platform()
-    .then(setPlatform)
-    .catch(console.error)
-
-  useEffect(() => {
-    if (platform == null) return
-    if (platform !== 'windows') return
-
-    path.dataDir()
-      .then(dataDir => {
-        const logpath = [dataDir.replace('Roaming','LocalLow'), 'Good Luck Games', 'Storybook Brawl', 'Player.log'].join(path.sep)
-        setLogFile(logpath)
-      })
+  useInterval(() => {
+    fs.readTextFile(path)
+      .then(setText)
       .catch(console.error)
-  }, [platform])
+  }, pollseconds * 1000)
+
+  return text
+}
+
+const useInterval = (callback: () => void, delay: number) => {
+  const savedCallback = useRef(callback)
 
   useEffect(() => {
-    if (logFile == null) return
-    if (logTimeout !== undefined) return
+    savedCallback.current = callback
+  }, [callback])
 
-    setLogTimeout(
-      setInterval(() => {
-        fs.readTextFile(logFile)
-          .then(setText)
-          .catch(console.error)
-      }, 60 * 1000)
-    )
-  }, [logFile, logTimeout])
+  useEffect(() => {
+    if (!delay && delay !== 0) return
+    const id = setInterval(() => savedCallback.current(), delay)
+    return () => clearInterval(id)
+  }, [delay])
+}
+
+const useRecords = () => {
+  const [records, setRecords] = useState<Game[]>()
+
+  const [logpath, setLogpath] = useState('')
+  useLogpath().then(setLogpath)
+
+  const text = useReadloop(logpath, 60)
 
   useEffect(() => {
     if (text == null) return
@@ -100,10 +114,12 @@ function App (): ReactElement {
     ))
   }, [text])
 
-  useEffect(() => {
-    if (records == null) return
-    setCurrent(records?.reduce((sum, { mmr }) => mmr + sum, startingMMR))
-  }, [records, startingMMR])
+  return records
+}
+
+function App(): ReactElement {
+  const [startingMMR, setStartingMMR] = useState(0)
+  const records = useRecords()
 
   return (
     <div className='app'>
@@ -113,7 +129,7 @@ function App (): ReactElement {
 
       <section className='mmr' style={{ flex: 1, flexDirection: 'row' }}>
         <StartingMMR onChange={setStartingMMR} />
-        <CurrentMMR mmr={current ?? 0} />
+        <CurrentMMR startingMMR={startingMMR} records={records} />
       </section>
 
       <section className='scores'>
